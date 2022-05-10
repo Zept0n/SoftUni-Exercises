b4ecd52cc401e57a5783a96d6d2ad0b0fcc0e6a9
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-05-10 23:58:48 +0300

    Added some files.

--- JavaScript Fundamentals/01.JS-Fundamentals-Basic-Syntax-Conditional-Statements-and-Loops-Exercise/01.Ages.js
+++ JavaScript Fundamentals/01.JS-Fundamentals-Basic-Syntax-Conditional-Statements-and-Loops-Exercise/01.Ages.js
@@ -1,1 +1,27 @@
+function ages(age) {
+    if (age>=0 && age<=2) {
+        console.log('baby');
+    }
+    else if(age>2&&age<=13) {
+        console.log('child');
+    }
+    else if(age>13&&age<=19) {
+        console.log('teenager');
+    }
+    else if(age>19&&age<=65) {
+        console.log('adult');
+    }
+    else if(age>65) {
+        console.log('elder');
+    }
+    else {
+        console.log('out of bounds');
+    }
+}
 
+ages(2);
+ages(20);
+ages(66);
+ages(55);
+ages(14);
+ages(-1);
---
+++ JavaScript Fundamentals/01.JS-Fundamentals-Basic-Syntax-Conditional-Statements-and-Loops-Exercise/04.Vacation.js
@@ -0,0 +1,48 @@
+function vacation(group, type, day) {
+    let price = 0;
+    let sum = 0;
+ 
+ 
+    if (day === "Friday") {
+        if (type === "Students") {
+            price = 8.45;
+        } else if (type === "Business") {
+            price = 10.90;
+        } else if (type === "Regular") {
+            price = 15;
+        }
+    }
+ 
+    if (day === "Saturday") {
+        if (type === "Students") {
+            price = 9.80;
+        } else if (type === "Business") {
+            price = 15.60;
+        } else if (type === "Regular") {
+            price = 20;
+        }
+    }
+ 
+    if (day === "Sunday") {
+        if (type === "Students") {
+            price = 10.46;
+        } else if (type === "Business") {
+            price = 16;
+        } else if (type === "Regular") {
+            price = 22.50;
+        }
+    }
+    sum = group * price;
+ 
+    if(group >= 30 && type === "Students"){
+        sum = sum - (sum * 0.15);   
+    }
+    if(group >= 100 && type === "Business"){
+        sum = sum - (10 * price)
+    }
+    if(group >= 10 && group <= 20 && type === "Regular"){
+        sum = sum - (sum * 0.05)
+    }
+    console.log(`Total price: ${sum.toFixed(2)}`);
+}
+vacation(40, "Regular", "Saturday")
