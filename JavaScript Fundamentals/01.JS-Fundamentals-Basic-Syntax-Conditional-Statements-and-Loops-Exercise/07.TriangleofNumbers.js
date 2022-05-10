dab7cb4c655007332ff62040f0c6cc84c513452a
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-05-11 00:04:39 +0300

    Added triangle of numbers.

---
+++ JavaScript Fundamentals/01.JS-Fundamentals-Basic-Syntax-Conditional-Statements-and-Loops-Exercise/07.TriangleofNumbers.js
@@ -0,0 +1,14 @@
+function triangle(number) {
+    for (let index = 1; index <= number; index++) {
+        let stringOfNum = '';
+        for (let j = 1; j <= index; j++) {
+            stringOfNum += index;
+            if (j !== index) {
+                stringOfNum += " ";
+            }
+        }
+        console.log(stringOfNum);
+    }
+}
+
+triangle(3);
