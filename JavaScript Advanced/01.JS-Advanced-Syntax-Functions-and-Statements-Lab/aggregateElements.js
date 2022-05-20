bf67b3b9c862a4d1cd15758cbfc77a5a0f315942
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-05-20 11:02:19 +0300

    Added another way to solve.

--- JavaScript Advanced/01.JS-Advanced-Syntax-Functions-and-Statements-Lab/aggregateElements.js
+++ JavaScript Advanced/01.JS-Advanced-Syntax-Functions-and-Statements-Lab/aggregateElements.js
@@ -1,4 +1,4 @@
-function solve(input) {
+/* function solve(input) {
     let Sum=0;
     let inverseSum=0
     let concString='';
@@ -9,4 +9,21 @@ function solve(input) {
     }
     console.log(`${Sum}\n${inverseSum}\n${concString}`);
 }
-solve([1,2,3]);
+solve([1,2,3]);  */
+
+
+function aggregateElements(elements) {
+    aggregate(elements, 0, (a, b) => a + b);
+    aggregate(elements, 0, (a, b) => a + 1 / b);
+    aggregate(elements, '', (a, b) => a + b);
+
+    function aggregate(arr, initVal, func) {
+        let val = initVal;
+        for (let i = 0; i < arr.length; i++) {
+            val = func(val, arr[i]);
+        }
+        console.log(val);
+    }
+}
+
+aggregateElements([1, 2, 3]); 
