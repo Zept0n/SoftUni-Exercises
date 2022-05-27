fb7db620b3354ab0eb76febda8a265bb159c9669
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-05-27 11:19:49 +0300

    Added file.

---
+++ JavaScript Fundamentals/11.ProblemsFinalExam/demo.js
@@ -0,0 +1,13 @@
+/* let a=5;
+let b=a++
+let c=++a
+console.log(c)
+
+let name="George";
+console.log(name.charAt(2)) */
+
+let a=10;
+let b=20;
+let c=a>b?a:b;
+let greed="Hello World";
+console.log(greed.substring(3,5))
