69fbe73fa3dfe16032e980ff335c8037b6ae9605
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-06-04 15:25:01 +0300

    Added file.

---
+++ JavaScript Advanced/03.JS-Advanced-Objects-and-Composition-Exercise/constructionCrew.js
@@ -0,0 +1,31 @@
+function crew(worker) {
+    if (worker.dizziness==true) {
+        let ammountToIntake=(worker.weight*0.1)*worker.experience
+        worker[`levelOfHydrated`]+=ammountToIntake;
+        worker[`dizziness`]=false;
+    }
+    return worker;
+}
+
+crew({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+}
+);
+
+crew({
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true
+}
+);
+crew({
+    weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false
+}
+);
