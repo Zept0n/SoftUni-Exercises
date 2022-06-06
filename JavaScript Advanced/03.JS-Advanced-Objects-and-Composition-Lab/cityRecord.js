88de22d48c299b1d44ab9b7242068165b176e3ba
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-06-06 11:01:18 +0300

    Added file.

---
+++ JavaScript Advanced/03.JS-Advanced-Objects-and-Composition-Lab/cityRecord.js
@@ -0,0 +1,17 @@
+function record(name,population,treasury) {
+    let city={
+        name,
+        population,
+        treasury
+    }
+
+    return city;
+}
+record('Tortuga',
+7000,
+15000
+);
+record('Santo Domingo',
+12000,
+23500
+);
