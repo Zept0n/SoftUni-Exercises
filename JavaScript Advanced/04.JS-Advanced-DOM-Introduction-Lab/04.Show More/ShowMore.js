42b1f428fdbdff0bc07d0974ad108c2700c60aee
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-06-07 12:44:05 +0300

    Added files.

---
+++ JavaScript Advanced/04.JS-Advanced-DOM-Introduction-Lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const clicker=document.getElementById('more');
+    const info=document.getElementById(`text`);
+    clicker.style.display='none';
+    info.style.display='inline';
+}
