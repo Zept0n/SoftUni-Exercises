5fb720863e51cb526b5a796f3c734231aaa0710b
Zept0n <104791727+Zept0n@users.noreply.github.com>
2022-06-14 09:57:58 +0300

    Added file.

---
+++ JavaScript Advanced/06.JS-Advanced-Advanced-Functions-Lab/commandProc.js
@@ -0,0 +1,11 @@
+
+//Closure?
+function proc() {
+    let str='';
+    return {
+        append:(s)=>str+=s,
+        removeStart:(n)=>str=str.substring(n),
+        removeEnd:(n)=>str=str.substring(0,str.length-n),
+        print:()=>console.log(str)
+    }
+}
